7e37010d7ecad7445fd5c295cfed8831f3a71705
Ujjwal Kumar Singh <95489300+theneuralcraftsman@users.noreply.github.com>
2023-11-13 22:57:45 +0530

    Update HelloWorld.py

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,6 +1,5 @@
-import tensorflow as tf
 import streamlit as st
-import pickle
+
 
 # model = tf.keras.models.load_model("D:/DeepLearningModels/Streamlit/linearModel.h5")
 # pickle.dump(model, open('D:/DeepLearningModels/Streamlit/model2','wb'))
