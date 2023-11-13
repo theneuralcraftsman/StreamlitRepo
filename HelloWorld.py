3750d77881fcbfd086690ed8a8c9d1d56bf4ce67
Ujjwal Kumar Singh <95489300+theneuralcraftsman@users.noreply.github.com>
2023-11-14 00:38:11 +0530

    Delete HelloWorld.py

--- HelloWorld.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-import tensorflow as tf
-
-# model = tf.keras.models.load_model("D:/DeepLearningModels/Streamlit/linearModel.h5")
-# pickle.dump(model, open('D:/DeepLearningModels/Streamlit/model2','wb'))
-
-st.title("Hello")
