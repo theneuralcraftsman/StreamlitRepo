a59cb0b4d29fc7f49ead97c331669188b31de773
Ujjwal Kumar Singh <95489300+theneuralcraftsman@users.noreply.github.com>
2023-11-13 22:54:08 +0530

    Update HelloWorld.py

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,5 +1,20 @@
+import tensorflow as tf
 import streamlit as st
+import pickle
 
-st.title("Hello World")
-st.header("Header")
-st.text("Hi")
+# model = tf.keras.models.load_model("D:/DeepLearningModels/Streamlit/linearModel.h5")
+# pickle.dump(model, open('D:/DeepLearningModels/Streamlit/model2','wb'))
+
+model = pickle.load(open('model2','rb'))
+
+st.title("Number predictor :one:")
+
+input_num = st.number_input('Input number: ')
+
+
+def predict():
+    output = model.predict([input_num])
+    st.success(f"The predicted number is: {output[0][0]:.2f}")
+    
+
+st.button('Predict', on_click=predict)
